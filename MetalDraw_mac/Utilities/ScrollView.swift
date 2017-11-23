f76a4bff5d296ba4fbbd17abc062671fe4a6da18
Adam Nemecek <adamnemecek@gmail.com>
2017-11-23 09:49:02 -0800

    * added scrollview & scrollviewdelegate

---
+++ MetalDraw_mac/Utilities/ScrollView.swift
@@ -0,0 +1,41 @@
+//
+//  ScrollView.swift
+//  MetalDraw_mac
+//
+//  Created by Kaz Yoshikawa on 11/23/17.
+//
+
+import Cocoa
+
+public protocol ScrollViewDelegate : class {
+    func scrollViewDidChange(_ scrollView : ScrollView)
+}
+
+public class ScrollView : NSScrollView {
+    public weak var delegate : ScrollViewDelegate? = nil
+
+    public override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        _config()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        _config()
+    }
+
+    @objc private func _observeScrolling(_ notification : Notification) {
+        delegate?.scrollViewDidChange(self)
+    }
+
+    private func _config() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(_observeScrolling),
+                                               name: NSView.boundsDidChangeNotification,
+                                               object: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
